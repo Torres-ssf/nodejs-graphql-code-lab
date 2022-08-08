5ef67e69bab7b15f82f0fce1df2802491e1c278b
Torres-ssf <sergio.uft@gmail.com>
2022-08-08 12:31:02 -0300

    properly set up app entry file

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,34 @@
+import 'reflect-metadata'
+import { ApolloServer } from 'apollo-server-express'
 import express from 'express'
+import { buildSchema } from 'type-graphql'
+import { UsersResolver } from './modules/users/graphql/resolvers/UsersResolver'
+import { connect } from './config/database'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.send('Hello World!')
-})
+async function init () {
+  await connect()
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000 🚀')
-})
+  const schema = await buildSchema({
+    resolvers: [UsersResolver]
+  })
+
+  const apolloServer = new ApolloServer({
+    schema
+  })
+
+  await apolloServer.start()
+
+  apolloServer.applyMiddleware({ app })
+
+  app.get('/', (req, res) => {
+    return res.send('Hello World!')
+  })
+
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000 🚀')
+  })
+}
+
+init()
