ee4a6f691ff6eb2c18a340c139d3b9acfd248a61
Torres-ssf <sergio.uft@gmail.com>
2022-08-08 11:24:04 -0300

    add entity class for User

---
+++ src/modules/users/database/entities/User.ts
@@ -0,0 +1,44 @@
+
+import {
+  BaseEntity,
+  BeforeInsert,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn
+} from 'typeorm'
+import bcrypt from 'bcrypt'
+import { Field, ID, ObjectType } from 'type-graphql'
+import { Pet } from '../../../pets/database/entities/Pet'
+
+@Entity('users')
+@ObjectType()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+    id: string
+
+  @Column()
+  @Field()
+    firstname: string
+
+  @Column()
+  @Field()
+    lastname: string
+
+  @Column({ unique: true })
+  @Field()
+    email: string
+
+  @Field(() => [Pet])
+    pets: Pet[]
+
+  password: string
+
+  @Column()
+    password_hash: string
+
+  @BeforeInsert()
+  private async hashPassword () {
+    this.password_hash = await bcrypt.hash(this.password, 8)
+  }
+}
